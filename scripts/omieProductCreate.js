b445023e4f941ed517c594213d6d14a532ed83fc
Guilherme Oliveira <guilherme@velabikes.com>
2019-12-02 17:02:17 -0300

    Update script create product firebase/omie

--- scripts/omieProductCreate.js
+++ scripts/omieProductCreate.js
@@ -9,19 +9,41 @@ const shopify = new createClient({ // eslint-disable-line
 })
 
 const handle = async () => {
-  // get shopify products
-  const quadroBaixo = await shopify.get('/admin/products/3728611573815.json').then(console.log).catch(console.error)
-
-  // parse shopify to firebase
+  const id = '' // quadro reto 4099129638967 - quadro baixo 3728611573815
+  const quadroBaixo = await shopify.get(`/admin/products/${id}.json`).catch(console.error)
   const products = firestore.collection('products')
-  quadroBaixo.variants.map(async prod => {
-    await products.set({ prod })
+
+  quadroBaixo.product.variants.map(async prod => {
+    const existingProduct = await products.where('id', '==', prod.id).limit(1).get()
+
+    if (existingProduct.empty) {
+      console.log(`Adding product ${prod.id}.`)
+
+      return (
+        products.doc().set({
+          id: prod.id,
+          inventory_quantity: prod.inventory_quantity,
+          sku: prod.sku,
+          title: prod.title
+        }).catch(console.error)
+      )
+    } else {
+      console.log(`Product ${prod.id} already exists, updating.`)
+
+      return (
+        existingProduct.docs[0].ref.update({
+          id: prod.id,
+          inventory_quantity: prod.inventory_quantity,
+          sku: prod.sku,
+          title: prod.title
+        }).catch(console.error)
+      )
+    }
   })
 
-  // parse firebase to omie
-  products.map(async prod => {
-    await addProduct({ prod })
-  })
+  // products.listDocuments.map(prod => {
+  //   addProduct({ ...prod })
+  // })
 }
 
 handle()
